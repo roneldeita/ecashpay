025c43e3b4961133674a5bca578a79d90c99b3d7
Ronel Deita <ortigas.ronel@gmail.com>
2018-02-15 18:39:41 +0800

    ecashpay is ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>main</p>
+        navigation
+        {this.props.children}
       </div>
     );
   }
--- src/routes.js
+++ src/routes.js
@@ -3,19 +3,20 @@ import PropTypes from 'prop-types'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import App from './App'
+import HomePage from './components/home/HomePage'
 import NotFound from './components/notFound'
 import Cart from './components/cart'
 
 const Routes = ({ store }) => (
   <Provider store={store}>
     <Router>
-      <div>
+      <App>
         <Switch>
-          <Route exact path="/" component={App} />
+          <Route exact path="/" component={HomePage} />
           <Route path="/cart" component={Cart} />
           <Route path="*" component={NotFound} />
         </Switch>
-      </div>
+      </App>
     </Router>
   </Provider>
 )
